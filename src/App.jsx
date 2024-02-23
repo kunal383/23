07dcd8b53e3912c4d52c985929d8733030695f3b
kunal383 <kunalmarwaha807@gmail.com>
2024-02-23 14:32:06 +0530

    mern 23-02-2024

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Aboutus from './assets/Aboutus'
+import File from './assets/File'
+
+export default function App() {
+  return (
+    <div>
+      <Aboutus/>
+      
+      
+    </div>
+  )
+}
+
+
